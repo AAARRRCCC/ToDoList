48d2dba710d4c84dccce037b1e71078568c77b1e
Ryan Brady <rbrady.business@gmail.com>
2023-05-17 22:59:09 -0400

    functionality is there but it looks bad

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,8 @@ fn main() {
             }
             "list" => {
                 //print a number followed by the TODO for that index of the vector
+                println!("Welcome to your TODO list! You have {} tasks remaining.", tasks.len());
+
                 for (index, task) in tasks.iter().enumerate(){
                     println!("{} {}", index + 1, task);
                 }
@@ -40,6 +42,15 @@ fn main() {
             }
             "-" => {
                 println!("Please specify which number TODO you would like to remove");
+                let mut index = String::new();
+                io::stdin().read_line(&mut index)
+                    .expect("Failed to read");
+                let index: usize = match index.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => continue,
+                };
+
+                removeTODO(index - 1, &mut tasks);
 
             }
             _ => {
@@ -48,6 +59,12 @@ fn main() {
 
 
         }
+
+        println!("/ / / / / / / / / / / / / / / / / / / / / / / / / / / / ");
+        println!("Please input '+' to add a new TODO entry");
+        println!("Please input 'list' to list current TODO entries");
+        println!("Please input '-' to delete a TODO entry @ pos");
+        println!("/ / / / / / / / / / / / / / / / / / / / / / / / / / / / ");
 
 
     }
@@ -58,4 +75,7 @@ fn addTODO(added: String, target: &mut Vec<String>){
     let todo = added;
 
     target.push(todo);
+}
+fn removeTODO(index: usize, target: &mut Vec<String>){
+    target.remove(index);
 }
